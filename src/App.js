e5c12583de8c0fa8982d2a6ccf81f26727bd7e27
debasish-afk <debasishbehera904@gmail.com>
2023-01-14 19:06:25 +0530

    reduce length

--- src/App.js
+++ src/App.js
@@ -4,24 +4,50 @@ const App = () => {
   const [fullName, setFullName] = useState({
     fname: "",
     lname: "",
+    gmail: "",
+    phone: "",
   });
 
   const inputEvent = (event) => {
-    const value = event.target.value;
-    const name = event.target.name;
+    // const value = event.target.value;
+    // const name = event.target.name;
+
+    const {value,name}=event.target
 
     setFullName((preValue) => {
-      if (name === "fname") {
-        return {
-          fname: value,
-          lname: preValue.lname,
-        };
-      } else if (name === "lname") {
-        return {
-          fname: preValue.fname,
-          lname: value,
-        };
+      return {
+        ...preValue,
+        [name]:value
       }
+      // if (name === "fname") {
+      //   return {
+      //     fname: value,
+      //     lname: preValue.lname,
+      //     gmail: preValue.gmail,
+      //     phone: preValue.phone,
+      //   };
+      // } else if (name === "lname") {
+      //   return {
+      //     fname: preValue.fname,
+      //     lname: value,
+      //     gmail: preValue.gmail,
+      //     phone: preValue.phone,
+      //   };
+      // } else if (name === "gmail") {
+      //   return {
+      //     fname: preValue.fname,
+      //     lname: preValue.lname,
+      //     gmail: value,
+      //     phone: preValue.phone,
+      //   };
+      // } else if (name === "phone") {
+      //   return {
+      //     fname: preValue.fname,
+      //     lname: preValue.lname,
+      //     gmail: preValue.gmail,
+      //     phone: value,
+      //   };
+      // }
     });
   };
 
@@ -36,7 +62,7 @@ const App = () => {
         <form onSubmit={onSubmit}>
           <div>
             <h1>
-              Hello {fullName.fname} {fullName.lname}
+              Hello {fullName.fname} {fullName.lname} {fullName.gmail} {fullName.phone}
             </h1>
             <input
               type="text"
@@ -54,6 +80,26 @@ const App = () => {
               onChange={inputEvent}
               value={fullName.lname}
             />
+            <br />
+            <br />
+            <input
+              type="text"
+              placeholder="Enter your Gmail"
+              name="gmail"
+              onChange={inputEvent}
+              value={fullName.gmail}
+            />
+            <br />
+            <br />
+            <input
+              type="number"
+              placeholder="Enter your Phone No."
+              name="phone"
+              onChange={inputEvent}
+              value={fullName.phone}
+            />
+            <br />
+            <br />
             <button type="submit">Submit Me</button>
           </div>
         </form>
